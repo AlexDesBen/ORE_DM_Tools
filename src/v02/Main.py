ef7c8d980f1292338735f189252e65f974c5f829
AlexDesBen <alexandre.desilets.benoit@gmail.com>
2017-05-22 11:52:15 -0400

    start of version 2

---
+++ src/v02/Main.py
@@ -0,0 +1,19 @@
+#*-* coding: utf-8*-*
+'''
+################################################################################
+################################################################################
+Author : Alexandre Desilets-Benoit
+Date : 2017-05-21
+Company : None
+Name of module : Onre Roll Engine Storyteller tools
+Version : 2.0
+Description : support tools for DM in ORE
+################################################################################
+################################################################################
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
